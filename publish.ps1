9d1cd8c895f20afa93cbb0b6a0e83f2e59907be6
senketsu03 <you@example.com>
2023-05-16 15:48:11 +0900

    Simplify publish script

--- publish.ps1
+++ publish.ps1
@@ -1,15 +1,19 @@
+echo "Publishing native binaries for:"
+
 if ($IsWindows)
 {
     $r = "win-x64"
-    dotnet publish "Deploy.Cli/Deploy.Cli.csproj" -c Release -r $r -o "publish/$r" --sc false
 }
-
-if ($IsLinux)
+elseif ($IsLinux)
 {
-
+    $r = "linux-x64"
 }
-
-if ($IsMacOS)
+elseif ($IsMacOS)
 {
-
+    $r = "osx-x64"
 }
+
+echo $r
+dotnet publish "Deploy.Cli/Deploy.Cli.csproj" -c Release -r $r -o "publish/$r" --sc false
+
+echo "Publish finished"
