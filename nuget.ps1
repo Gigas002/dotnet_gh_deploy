60ffed9c04993631ea79c0230bb4e271c543f75f
senketsu03 <you@example.com>
2023-05-18 10:49:47 +0900

    Add nuget deployment script

---
+++ nuget.ps1
@@ -0,0 +1,16 @@
+$versionSuffix = "2"
+$apiKey = ""
+$src = "Deploy.Core/Deploy.Core.csproj"
+$packDir = "publish/Deploy.Core"
+
+dotnet build $src -c Release
+dotnet pack $src -c Release -o $packDir --no-build --version-suffix ci-$versionSuffix
+
+foreach($file in (Get-ChildItem $packDir -Recurse -Include *.nupkg))
+{
+    # see: https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-nuget-push
+    dotnet nuget push $file -k $apiKey -s https://api.nuget.org/v3/index.json --skip-duplicate
+
+    # for github package feed
+    # dotnet nuget push $file -s "github" -ss "github" --skip-duplicate
+}
