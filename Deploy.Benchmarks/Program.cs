de7ec816d093065a6a26a9f0888b682f1f71176a
senketsu03 <you@example.com>
2023-05-22 15:30:41 +0900

    Add benchmarks

---
+++ Deploy.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Deploy.Benchmarks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Md5VsSha256>();
+    }
+}
